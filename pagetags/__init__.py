8e877955a0b9d4689837186e1bd5721a0933922a
Panagiotis Matigakis <pmatigakis@gmail.com>
2016-08-15 17:25:44 +0300

    Added the setup.py file

---
+++ pagetags/__init__.py
@@ -0,0 +1,6 @@
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+manager = Manager()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Pagetags",
+    version = "0.1",
+    packages = find_packages(exclude=["tests"]),
+    install_requires = [
+        "Flask==0.11.1",
+        "Flask-Script==2.0.5",
+        "Flask-SQLAlchemy==2.1",
+        "Flask-WTF==0.12",
+        "Flask-Login==0.3.2"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=["nose==1.3.7"],
+    tests_require=["nose==1.3.7"],
+    test_suite = 'nose.collector',
+)
