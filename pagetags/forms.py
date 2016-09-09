84bdd051c3b7c8e6a52b373892db6ed8954fa011
Panagiotis Matigakis <pmatigakis@gmail.com>
2016-09-09 21:03:06 +0300

    The password field in the login form will not be visible when entering it

--- pagetags/forms.py
+++ pagetags/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, URL
 
 
@@ -11,4 +11,4 @@ class Url(Form):
 
 class Login(Form):
     username = StringField("username", validators=[DataRequired()])
-    password = StringField("password", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
